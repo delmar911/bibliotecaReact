bd5cac83c9131d2dfb41e6a7a3a96c10178c31db
Maria Del Mar <mariadelmar.artunduaga911@gmail.com>
2024-11-01 18:16:54 -0500

    inicio

---
+++ src/pages/Inicio.jsx
@@ -0,0 +1,23 @@
+export const Inicio = () => {
+   
+    return (
+      <main>
+        <div className='flex flex-col items-center justify-center w-full h-full p-8'>
+          <div className="">
+            <div className='flex flex-col w-full h-full mt-4 text-white ' >
+             Inicio
+              <p className='mt-4 font-medium text-center text-white text-3xl'>
+               Inicio
+              </p>
+            </div>
+          </div>
+          
+           
+          
+          
+        </div>
+      </main>
+    );
+  }
+  
+  export default Inicio;
